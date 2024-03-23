b6a940859100047954302e4d1aaf191b8dc8c200
Mateus de Sousa <mateus69107@gmail.com>
2024-03-23 17:59:49 -0300

    :sparkles: Models: Categoria e Produto

---
+++ Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUD_ASP.NEST_Core_8;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? ImagemURL { get; set; }
+
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace CRUD_ASP.NEST_Core_8;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public string? ImagemURL { get; set; }
+
+    public decimal Preco { get; set; }
+
+    public float Estoque { get; set; }
+
+    public DateTime DataCadastro { get; set; }
+
+}
